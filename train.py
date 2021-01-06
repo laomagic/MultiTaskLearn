339a71cf0f6cc9c85fcf79d9a756a98d3cf08286
laomagic <18710797707@163.com>
2021-01-06 14:33:25 +0800

    modify train

--- train.py
+++ train.py
@@ -1,8 +1,7 @@
 import os
-from transformers import BertTokenizer, BertModel
-from joint_dataset import JDataset, collate_fn
-from torch.utils.data import DataLoader
-from bert_model import JointModel
+from transformers import BertTokenizer, BertModel, get_linear_schedule_with_warmup
+from gen_datasets import GenDatasets
+from src.bert_model import JointModel
 import json
 import argparse
 import logging
@@ -15,22 +14,23 @@ from utils import save_model, load_model, create_logger, compute_loss, compute_s
 from itertools import zip_longest
 
 # 随机数固定
-seed = 2020
+seed = 1999
 torch.manual_seed(seed)
 torch.cuda.manual_seed(seed)
 torch.cuda.manual_seed_all(seed)
 np.random.seed(seed)
 # 日志记录
 root_path = os.path.abspath(os.path.dirname(__file__))
-logger = create_logger(log_path=root_path + "/logs/train_ratio_w.log")
+logger = create_logger(log_path=root_path + "/train_w.log")
 
 
-def train_model(args, dataloader, model, optimizer, epochs, master_gpu_id, use_cuda=False, gradient_accumulation_steps=1):
+def train_model(args, dataloader, scheduler, model, optimizer, epochs, master_gpu_id, use_cuda=False,
+                gradient_accumulation_steps=1):
     logger.info("Start Training")
     total_epoch = 0
     for epoch in range(1, epochs + 1):
         logger.info("Training Epoch:{}/{}".format(epoch, epochs))
-        train_epoch(model, optimizer, dataloader, master_gpu_id, gradient_accumulation_steps, use_cuda)
+        train_epoch(model, optimizer, scheduler, dataloader, master_gpu_id, gradient_accumulation_steps, use_cuda)
         logger.info("Eval" + "=" * 70)
         tnews_f1, tnews_loss = eval_model(args, model, dataloader, master_gpu_id, use_cuda, task_name="tnews")
         ocnli_f1, ocnli_loss = eval_model(args, model, dataloader, master_gpu_id, use_cuda, task_name='ocnli')
@@ -44,14 +44,14 @@ def train_model(args, dataloader, model, optimizer, epochs, master_gpu_id, use_c
         save_model(model, epoch, optimizer, loss, f1, args.save_model_path)
 
 
-def train_epoch(model, optimizer, dataloader, master_gpu_id, gradient_accumulation_steps, use_cuda):
+def train_epoch(model, optimizer, scheduler, dataloader, master_gpu_id, gradient_accumulation_steps, use_cuda):
     global global_step
     global_step = 0
     model.train()
     total_loss = 0.0
     total_f1 = 0.0
 
-    weights = json.load(open('data/label_weights.json', 'r', encoding="utf-8"))
+    weights = json.load(open(root_path + '/label_weights.json', 'r', encoding="utf-8"))
     tnews_weight = torch.tensor(weights["TNEWS"]).cuda(master_gpu_id) if use_cuda else torch.tensor(weights["TNEWS"])
     ocnli_weight = torch.tensor(weights["OCNLI"]).cuda(master_gpu_id) if use_cuda else torch.tensor(weights["OCNLI"])
     ocemotion_weight = torch.tensor(weights["OCEMOTION"]).cuda(master_gpu_id) if use_cuda else torch.tensor(
@@ -86,23 +86,24 @@ def train_epoch(model, optimizer, dataloader, master_gpu_id, gradient_accumulati
         loss.backward()  # 损失反向传播，计算梯度
         if (step + 1) % gradient_accumulation_steps == 0:
             optimizer.step()  # 梯度更新
+            # scheduler.step()
             optimizer.zero_grad()  # 梯度清零
 
         try:
             tnews_f1 = compute_score(*batch_output["TNEWS"])
         except:
             tnews_f1 = 0.0
-            logger.info("step:%s TNEWS is not exist" % step)
+            # logger.info("step:%s TNEWS is not exist" % step)
         try:
             ocnli_f1 = compute_score(*batch_output["OCNLI"])
         except:
             ocnli_f1 = 0.
-            logger.info("step:%s OCNLI is not exist" % step)
+            # logger.info("step:%s OCNLI is not exist" % step)
         try:
             ocemotion_f1 = compute_score(*batch_output["OCEMOTION"])
         except:
             ocemotion_f1 = 0.0
-            logger.info("step:%s OCEMOTION is not exist" % step)
+            # logger.info("step:%s OCEMOTION is not exist" % step)
 
         f1 = tnews_f1 + ocnli_f1 + ocemotion_f1
         total_loss += loss.item()
@@ -176,35 +177,13 @@ def main(args):
     tokenizer = BertTokenizer.from_pretrained(args.model_path)
     # 加载数据集
     logger.info("Dataset init")
+    genDataset = GenDatasets(tokenizer, batch_size=args.batch_size, sampler=args.sampler)
     dataloader = {"tnews": {},
                   "ocnli": {},
                   "ocemotion": {}}
     if args.train_data:
-        tnews_train_dataset = JDataset(tokenizer, task_name="TNEWS", mode="train")
-        ocnli_train_dataset = JDataset(tokenizer, task_name="OCNLI", mode="train")
-        ocemotion_train_dataset = JDataset(tokenizer, task_name="OCEMOTION", mode="train")
-        tnews_length = tnews_train_dataset.__len__()
-        ocnli_length = ocnli_train_dataset.__len__()
-        ocemotion_length = ocemotion_train_dataset.__len__()
-        length = tnews_length + ocnli_length + ocemotion_length
-        ocnli_ratio = round((ocnli_length / length) * args.batch_size)
-        ocemotion_ratio = round((ocemotion_length / length) * args.batch_size)
-        tnews_ratio = args.batch_size - ocemotion_ratio - ocnli_ratio
-        tnews_train_dataloader = DataLoader(tnews_train_dataset,
-                                            collate_fn=collate_fn,
-                                            batch_size=tnews_ratio,
-                                            shuffle=True,
-                                            num_workers=2)
-        ocnli_train_dataloader = DataLoader(ocnli_train_dataset,
-                                            collate_fn=collate_fn,
-                                            batch_size=ocnli_ratio,
-                                            shuffle=True,
-                                            num_workers=2)
-        ocemotion_train_dataloader = DataLoader(ocemotion_train_dataset,
-                                                collate_fn=collate_fn,
-                                                batch_size=ocemotion_ratio,
-                                                shuffle=True,
-                                                num_workers=2)
+        tnews_train_dataloader,ocnli_train_dataloader,ocemotion_train_dataloader = \
+            genDataset.tnews_train_dataloader, genDataset.ocnli_train_dataloader, genDataset.ocemotion_train_dataloader
 
         dataloader['tnews']["train"] = tnews_train_dataloader
         dataloader['ocnli']["train"] = ocnli_train_dataloader
@@ -214,15 +193,8 @@ def main(args):
         logger.info("Load Training Dataloader Done, Total training line: %s", ocemotion_train_dataloader.__len__())
 
     if args.eval_data:
-        tnews_dev_dataset = JDataset(tokenizer, task_name="TNEWS", mode="dev")
-        ocnli_dev_dataset = JDataset(tokenizer, task_name="OCNLI", mode="dev")
-        ocemotion_dev_dataset = JDataset(tokenizer, task_name="OCEMOTION", mode="dev")
-        tnews_dev_dataloader = DataLoader(tnews_dev_dataset, collate_fn=collate_fn, batch_size=args.batch_size,
-                                          shuffle=True)
-        ocnli_dev_dataloader = DataLoader(ocnli_dev_dataset, collate_fn=collate_fn, batch_size=args.batch_size,
-                                          shuffle=True)
-        ocemotion_dev_dataloader = DataLoader(ocemotion_dev_dataset, collate_fn=collate_fn, batch_size=args.batch_size,
-                                              shuffle=True)
+        tnews_dev_dataloader,ocnli_dev_dataloader,ocemotion_dev_dataloader = \
+            genDataset.tnews_dev_dataloader, genDataset.ocnli_dev_dataloader, genDataset.ocemotion_dev_dataloader
         dataloader['tnews']["dev"] = tnews_dev_dataloader
         dataloader['ocnli']["dev"] = ocnli_dev_dataloader
         dataloader['ocemotion']["dev"] = ocemotion_dev_dataloader
@@ -231,16 +203,19 @@ def main(args):
         logger.info("Load Evaling Dataloader Done, Total training line: %s", ocemotion_dev_dataloader.__len__())
 
     if args.run_mode == "train":
-        no_decay = ["bias", "gamma", "beta"]
-        optimizer_parameters = [
-            {"params": [p for name, p in model.named_parameters() \
-                        if name not in no_decay], "weight_decay_rate": 0.01},
-            {"params": [p for name, p in model.named_parameters() \
-                        if name in no_decay], "weight_decay_rate": 0.0}
-        ]
-        # optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
-        optimizer = torch.optim.Adam(optimizer_parameters, lr=args.lr)
+        # no_decay = ["bias", "gamma", "beta"]
+        # optimizer_parameters = [
+        #     {"params": [p for name, p in model.named_parameters() \
+        #                 if name not in no_decay], "weight_decay_rate": 0.01},
+        #     {"params": [p for name, p in model.named_parameters() \
+        #                 if name in no_decay], "weight_decay_rate": 0.0}
+        # ]
+
+        optimizer = torch.optim.Adam(model.parameters(), lr=args.lr)
+        scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0, num_training_steps=args.epochs)
+        # optimizer = torch.optim.AdamW(optimizer_parameters, lr=args.lr)
         train_model(args, dataloader,
+                    scheduler=scheduler,
                     model=model,
                     optimizer=optimizer,
                     epochs=args.epochs,
@@ -266,13 +241,12 @@ if __name__ == '__main__':
     parser.add_argument('--run_mode', dest="run_mode", action="store", default="train",
                         help="Running mode: train or eval")
     parser.add_argument("--batch_size", dest="batch_size", action="store", type=int, default=64, help="")
+    parser.add_argument("--sampler", dest="sampler", action="store", type=bool, default=True, help="数据采样")
     parser.add_argument("--task_name", dest="task_name", action="store", default='TNEWS,OCNLI,OCEMOTION', help="")
-    parser.add_argument("--epochs", dest="epochs", action="store", type=int, default=5, help="")
+    parser.add_argument("--epochs", dest="epochs", action="store", type=int, default=6, help="")
     parser.add_argument("--lr", dest="lr", action="store", type=float, default=0.0001, help="")
-    parser.add_argument("--save_model_path", dest="save_model_path", action="store", default="save_ratio_w",
-                        help="")
-    parser.add_argument("--model_path", dest="model_path", action="store", default="model/w_ratio_w/",
-                        help="pretrained model")
+    parser.add_argument("--save_model_path", dest="save_model_path", action="store", default="save_w", help="")
+    parser.add_argument("--model_path", dest="model_path", action="store", default="model/w", help="pretrained model")
     parser.add_argument("--log_path", dest="log_path", action="store", default="logs", help="")
     parser.add_argument("--gradient_accumulation_steps", dest="gradient_accumulation_steps", action="store",
                         type=int, default=16, help="")
