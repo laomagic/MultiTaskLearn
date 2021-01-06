1a6734e1757444540f153003dca4da074cb27173
laomagic <18710797707@163.com>
2021-01-06 14:44:56 +0800

    add focal

--- utils.py
+++ utils.py
@@ -147,3 +147,76 @@ def create_logger(log_path):
     logger.addHandler(th)
 
     return logger
+
+
+class CELoss(nn.Module):
+    def __init__(self, class_num, alpha=None, use_alpha=False, size_average=True):
+        super(CELoss, self).__init__()
+        self.class_num = class_num
+        self.alpha = alpha
+        if use_alpha:
+            self.alpha = torch.tensor(alpha).cuda()
+
+        self.softmax = nn.Softmax(dim=1)
+        self.use_alpha = use_alpha
+        self.size_average = size_average
+
+    def forward(self, pred, target):
+        prob = self.softmax(pred.view(-1, self.class_num))
+        prob = prob.clamp(min=0.0001, max=1.0)
+
+        target_ = torch.zeros(target.size(0), self.class_num).cuda()
+        target_.scatter_(1, target.view(-1, 1).long(), 1.)
+
+        if self.use_alpha:
+            batch_loss = - self.alpha.double() * prob.log().double() * target_.double()
+        else:
+            batch_loss = - prob.log().double() * target_.double()
+
+        batch_loss = batch_loss.sum(dim=1)
+
+        # print(prob[0],target[0],target_[0],batch_loss[0])
+        # print('--')
+
+        if self.size_average:
+            loss = batch_loss.mean()
+        else:
+            loss = batch_loss.sum()
+
+        return loss
+
+
+class FocalLoss(nn.Module):
+    def __init__(self, class_num, alpha=None, gamma=2, use_alpha=False, size_average=True):
+        super(FocalLoss, self).__init__()
+        self.class_num = class_num
+        self.alpha = alpha
+        self.gamma = gamma
+        if use_alpha:
+            self.alpha = torch.tensor(alpha).cuda()
+
+        self.softmax = nn.Softmax(dim=1)
+        self.use_alpha = use_alpha
+        self.size_average = size_average
+
+    def forward(self, pred, target):
+        prob = self.softmax(pred.view(-1, self.class_num))
+        prob = prob.clamp(min=0.0001, max=1.0)
+
+        target_ = torch.zeros(target.size(0), self.class_num).cuda()
+        target_.scatter_(1, target.view(-1, 1).long(), 1.)
+
+        if self.use_alpha:
+            batch_loss = - self.alpha.double() * torch.pow(1 - prob,
+                                                           self.gamma).double() * prob.log().double() * target_.double()
+        else:
+            batch_loss = - torch.pow(1 - prob, self.gamma).double() * prob.log().double() * target_.double()
+
+        batch_loss = batch_loss.sum(dim=1)
+
+        if self.size_average:
+            loss = batch_loss.mean()
+        else:
+            loss = batch_loss.sum()
+
+        return loss
